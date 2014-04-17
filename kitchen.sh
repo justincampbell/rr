c50645ec5b73304227e3ab5b04f2499d1fc9fc90
Justin Campbell <justin@justincampbell.me>
2014-04-17 08:40:59 -0400

    Confiure Test Kitchen

---
+++ kitchen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+package() {
+  if which apt-get; then
+    apt-get install $@ -y
+  elif which yum; then
+    yum install $@ -y
+  fi
+}
+
+package git-core make
+
+git clone https://github.com/sstephenson/bats.git /tmp/bats && \
+  cd /tmp/bats && \
+  ./install.sh /usr
+
+cd /tmp/kitchen/data
+make
